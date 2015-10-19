a1b8f10473033c21beda5adce4a09ce1a7e47bdf
Sky Winston <skywinston@gmail.com>
2015-10-19 17:04:58 -0600

    Expressified app for deployment to Heroku

--- app.js
+++ app.js
@@ -1,118 +1,63 @@
-// On DOM ready, add click Event Listeners to the canvas
-window.onload = function(){
-  var canvas = document.getElementById("canvas");
-  var primaryPalette = document.getElementById("color_palette_primary");
-  var toolPicker = document.getElementById("tool_picker");
-  canvas.addEventListener("mousedown", applyTool) //works for clicks
-  primaryPalette.addEventListener("click", chooseColor);
-  toolPicker.addEventListener("click", switchTool)
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+app.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
 }
 
-// Finds the active tool and applies its logic to the clicked pixel.
-function applyTool(event){
-  switch (workspace.activeTool) {
-    case "pixel_brush":
-      brushPixel(event);
-      break;
-
-    case "paint_tool":
-      dumpPaint(event);
-      break;
-
-    case "color_picker":
-      pickColor(event);
-      break;
-  }
-}
-
-// Choose a color from the default color palette
-function chooseColor(event){
-  var newColor = event.target.getAttribute("data-color");
-  workspace.currentColor = newColor;
-};
-
-// Switches the active tool in the workspace object.
-function switchTool(event){
-  var toolToEquip = event.target.getAttribute("data-tooltype");
-  workspace.activeTool = toolToEquip;
-}
-
-// Create global color object
-var workspace = {
-  currentColor: "bg-red-500",
-  activeTool: "pixel_brush"
-};
-
-// Queries the global color object for the current color, and applies it
-// to the element that was clicked.  If already set to currentColor, it
-// will toggle the pixel to the unpainted (default) state.
-function brushPixel(event){
-  $( "#canvas" ).selectable({
-    disabled: true
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
   });
-
-  addOrRemoveCurrentColor(event);
-
-  canvas.addEventListener("mouseover", dragToPaint);
-  document.addEventListener("mouseup", stopPainting);
-};
-
-function dragToPaint(event){
-  addCurrentColor(event);
-};
-
-function stopPainting(event){
-  canvas.removeEventListener("mouseover", dragToPaint);
-  document.removeEventListener("mouseup", dragToPaint);
-};
-
-function addOrRemoveCurrentColor(event) {
-  var colorToApply = workspace.currentColor;
-  var currentColor = event.target.getAttribute("data-color");
-  if (colorToApply !== currentColor){
-    event.target.classList.remove(event.target.getAttribute("data-color"))
-    event.target.setAttribute("data-color", colorToApply);
-    event.target.classList.add(colorToApply);
-  }
-
-  else {
-    event.target.setAttribute("data-color", "bg-default");
-    event.target.classList.remove(currentColor);
-    event.target.classList.add("bg-default");
-  }
-};
-
-function addCurrentColor(event) {
-    if (event.target.classList.contains(workspace.currentColor)) {
-      return;
-    }
-    event.target.classList.remove(event.target.getAttribute("data-color"));
-    event.target.setAttribute("data-color", workspace.currentColor);
-    event.target.classList.add(workspace.currentColor);
-};
-
-var dumpGrid = [];
-function dumpPaint(event){
-  $(function() {
-    $( "#canvas" ).selectable();
-  });
-
-  $( "#canvas" ).selectable({
-    disabled: false
-  });
-
-  $( "#canvas" ).on( "selectableselecting", function( event, ui ) {
-    $(ui.selecting).removeClass($(ui.selecting).attr('data-color'));
-    $(ui.selecting).attr('data-color', workspace.currentColor);
-    $(ui.selecting).addClass(workspace.currentColor);
-    dumpGrid.push(ui.selecting);
-  });
+});
 
 
-
-
-};
-
-function pickColor(event){
-  console.log("I'm picking a color now.");
-};
+module.exports = app;
